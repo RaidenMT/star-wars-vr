df72d2a51e17d04b998de639b1b3dd006c3c4ef2
RaidenMT <raikun96@gmail.com>
2021-01-15 10:20:01 -0400

    video of deathstar page trying to play when page is loaded

--- assets/js/deathstar.js
+++ assets/js/deathstar.js
@@ -1,18 +1,4 @@
 const videoDarthVader = document.querySelector("#darthvader");
-
-videoDarthVader.play();
-
-// const stormtroopers = document.querySelectorAll(".stormtrooper");
-
-// console.log(stormtroopers[1].components['gltf-model']);
-
-
-
-// for (var i = 0; i < stormtroopers.length; i++) {
-//     const stormtrooper = stormtroopers[i];
-//     if (stormtroopers[i].components["animation-mixer"] !== undefined) {
-//         stormtroopers[i].components["animation-mixer"].mixer.timeScale = 1;   
-//     }
-// }
-
-
+document.querySelector("a-scene").addEventListener('loaded', function() {
+    videoDarthVader.play();
+});
